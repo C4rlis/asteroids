511640cbd887722cfb7c859d30262f639e70f3a8
C4rlis <carlmarttila@gmail.com>
2025-07-24 19:27:49 +0300

    I think it works

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from constants import *
+from player import *
 import pygame
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt =+ (clock.tick()/1000)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
