a6c90116d1c3e9b01be29ff0a5b8f85a3232715b
C4rlis <carlmarttila@gmail.com>
2025-07-24 21:16:10 +0300

    Got roids maybe to work

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
+import sys
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
+
+    
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x,y)
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    asteroid_field = AsteroidField()
+
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
-        clock.tick(60)
-        dt =+ (clock.tick()/1000)
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
+                    asteroid.split()
+
+
+        screen.fill("black")
+
+        for obj in drawable:
+                obj.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
